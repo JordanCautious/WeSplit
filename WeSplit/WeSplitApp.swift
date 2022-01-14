3faf905709620d4863c58f6f29d1e8f4756ac3c3
Jordan Haynes <Jordancautious@gmail.com>
2022-01-14 11:00:19 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jordan Haynes on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
